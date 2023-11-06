bcac898aeb2eea2970711200a8f1e7b44d7a52ad
Ethan Tillotson <89542030+rllycool@users.noreply.github.com>
2023-11-06 12:36:33 -0600

    Add files via upload

--- ShiftReg.v
+++ ShiftReg.v
@@ -5,18 +5,21 @@ module ShiftReg(
 input wire enable,
 input wire clk,
 input wire [7:0] ascii,
-output reg out
+output reg [7:0] out
 
 );
-
+parameter N = 10;
 reg [9:0] shift;
 
-always @ (posedge enable) begin
-
-	shift <= {1'b1, ascii, 1'b0};
+always @ (posedge clk) begin
 	
-	shift <= {1'b1, shift[9:1]}; //shift 1 to MSB
+	if(enable) begin
+		shift[N-1:1] <= shift[N-2:0]; 
+		shift[0] <= 0;
+	end
+	else begin
+		shift <= {1'b1, ascii, 1'b0};
+	end
 	out <= shift[8:1];
-
 end
 endmodule
